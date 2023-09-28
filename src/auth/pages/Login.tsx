21600460dff9ebf9887a1d933b39b8306f164fb5
HappyMan17 <sebasgrisales16@gmail.com>
2023-09-28 09:52:02 -0500

    fix(Eslint): ESlinter configuration fixed and now working propertly.

--- src/auth/pages/Login.tsx
+++ src/auth/pages/Login.tsx
@@ -1,5 +1,6 @@
-import { Box, createTheme, Grid, TextField, ThemeProvider, Button, Link} from "@mui/material";
-import Typography from '@mui/material/Typography';
+import React from 'react'
+import { Box, createTheme, Grid, TextField, ThemeProvider, Button, Link } from '@mui/material'
+import Typography from '@mui/material/Typography'
 
 function Copyright(props: any) {
   return (
@@ -10,7 +11,7 @@ function Copyright(props: any) {
       {new Date().getFullYear()}
       {'.'}
     </Typography>
-  );
+  )
 }
 
 const defaultTheme = createTheme();
@@ -22,62 +23,61 @@ export const Login = () => {
     const data = new FormData(event.currentTarget);
     console.log({
       email: data.get('email'),
-      password: data.get('password'),
+      password: data.get('password')
     });
-
   };
-  
+
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Grid container sx={{display: 'flex', direction: 'row', margin: '0'}}>
-        <Grid container name="derecha" sx={{
-          background: 'linear-gradient(45deg, #253469, #525D93, #111E31, #230E30)', 
+      <Grid container sx={{ display: 'flex', direction: 'row', margin: '0' }} >
+        <Grid container sx={{
+          background: 'linear-gradient(45deg, #253469, #525D93, #111E31, #230E30)',
           backgroundRepeat: 'no-repeat',
           backgroundSize: 'cover',
           display: 'flex',
           justifyContent: 'center',
           alignItems: 'center',
           width: '100%',
-          height: '100vh',
+          height: '100vh'
         }}>
-          <Grid item xs={12} sm={8} elavation={6} name="izquierda" sx={{
-              bgcolor: 'white',
-              height: {sm: '500px', xs: '100vh'},
-              maxWidth: '500px',
-              minWidth: '300px',
-              borderRadius: {sm: 8, xs: 0},
-            }}>
-            <Box sx={{ 
+          <Grid item xs={12} sm={8} sx={{
+            bgcolor: 'white',
+            height: { sm: '500px', xs: '100vh' },
+            maxWidth: '500px',
+            minWidth: '300px',
+            borderRadius: { sm: 8, xs: 0 }
+          }}>
+            <Box sx={{
               my: 8,
               mx: 4,
               display: 'flex',
               flexDirection: 'column',
               alignItems: 'center',
-              justifyContent: 'center',
+              justifyContent: 'center'
             }}>
             <Typography component="h1" variant="h5">Login</Typography>
-              <Box component="form" noValidate onSubmit={onSubmit} sx={{mt:1}}>
+              <Box component="form" noValidate onSubmit={onSubmit} sx={{ mt: 1 }}>
                 <TextField
-                  margin="normal" 
-                  required fullWidth 
-                  id="email" 
-                  label="Email" 
-                  name="email" 
-                  autoComplete="email" 
+                  margin="normal"
+                  required fullWidth
+                  id="email"
+                  label="Email"
+                  name="email"
+                  autoComplete="email"
                   autoFocus
                 />
-                <TextField 
-                  margin="normal" 
-                  required fullWidth 
-                  id="password" 
-                  label="Contraseña" 
+                <TextField
+                  margin="normal"
+                  required fullWidth
+                  id="password"
+                  label="Contraseña"
                   type="password"
-                  name="password" 
+                  name="password"
                 />
-                <Button 
+                <Button
                   type="submit"
                   fullWidth variant="contained"
-                  sx={{mt:3, mb:2}}
+                  sx={{ mt: 3, mb: 2 }}
                 >
                   SING IN
                 </Button>
@@ -89,7 +89,7 @@ export const Login = () => {
                     <Link href="#" variant="body2">¿No tienes cuenta? Registrate</Link>
                   </Grid>
                 </Grid>
-                <Copyright sx={{mt:5}} />
+                <Copyright sx={{ mt: 5 }} />
               </Box>
             </Box>
           </Grid>
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { createRoot } from 'react-dom/client';
 import './index.css'
 import { AppRouter } from './router/'
 import { BrowserRouter } from 'react-router-dom'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <AppRouter />
     </BrowserRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
