16a8118d79709c1f3bbe21c2ce6ccf8e71f5e2c3
HappyMan17 <sebasgrisales16@gmail.com>
2023-10-24 19:13:12 -0500

    feat(PagesRouter): Lazy loading implemented in the router layout.

--- src/app/router/PagesRouter.tsx
+++ src/app/router/PagesRouter.tsx
@@ -1,22 +1,9 @@
 import { Outlet, Route, Routes } from 'react-router-dom';
-// import { UserHome } from '../pages/user_home/UserHome';
 import Navbar from '../../components/navbar/Navbar';
 import UserHome from '../pages/user_home/UserHome';
 import { userHomeNavItems } from '../../components/data';
-// import { AddContactForm, AddEventForm, DeleteContactForm, InactivateProfileForm, ModifyEventForm, ModifyProfileForm } from '../../components';
 import { Suspense, lazy } from 'react';
 import { Grid } from '@mui/material';
-
-const Layout = () => {
-  return (
-    <>
-      <Navbar props={{ navbarItem: userHomeNavItems, sideBarItems: userHomeNavItems }} >
-        <Outlet />
-      </Navbar>
-    </>
-  )
-}
-
 const ModifyProfileForm = lazy(async () => await import('../../components/event_items/ModifyEventForm'));
 const InactivateProfileForm = lazy(async () => await import('../../components/event_items/AddEventForm'));
 const AddContactForm = lazy(async () => await import('../../components/contacts_items/AddContactForm'));
@@ -24,17 +11,27 @@ const DeleteContactForm = lazy(async () => await import('../../components/contac
 const AddEventForm = lazy(async () => await import('../../components/event_items/AddEventForm'));
 const ModifyEventForm = lazy(async () => await import('../../components/event_items/ModifyEventForm'));
 
+const Layout = () => {
+  return (
+    <Suspense fallback={<Grid>Loading...</Grid>}>
+      <Navbar props={{ navbarItem: userHomeNavItems, sideBarItems: userHomeNavItems }} >
+        <Outlet />
+      </Navbar>
+    </Suspense>
+  )
+}
+
 export const PagesRouter = () => {
   return (
     <Routes>
         <Route path='/' element={ <Layout /> } >
           <Route path="/" element={ <UserHome /> } />
-          <Route path="modify-account" element={ <Suspense fallback={<Grid>Loading...</Grid>}><ModifyProfileForm /></Suspense> } />
-          <Route path="inactivate-account" element={ <Suspense fallback={<Grid>Loading...</Grid>}><InactivateProfileForm /></Suspense> } />
-          <Route path="add-contact" element={ <Suspense fallback={<Grid>Loading...</Grid>}><AddContactForm /></Suspense> } />
-          <Route path="delete-contact" element={ <Suspense fallback={<Grid>Loading...</Grid>}><DeleteContactForm /></Suspense> } />
-          <Route path="add-event" element={ <Suspense fallback={<Grid>Loading...</Grid>}><AddEventForm /></Suspense> } />
-          <Route path="modify-event" element={ <Suspense fallback={<Grid>Loading...</Grid>}><ModifyEventForm /></Suspense> } />
+          <Route path="modify-account" element={ <ModifyProfileForm /> } />
+          <Route path="inactivate-account" element={ <InactivateProfileForm /> } />
+          <Route path="add-contact" element={ <AddContactForm /> } />
+          <Route path="delete-contact" element={ <DeleteContactForm /> } />
+          <Route path="add-event" element={ <AddEventForm /> } />
+          <Route path="modify-event" element={ <ModifyEventForm /> } />
           {/* Defult page */}
           <Route path="/*" element={ <UserHome /> } />
         </Route>
