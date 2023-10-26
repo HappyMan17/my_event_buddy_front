e241eb3eb2866bd9a88024a84b7ffceddc7f6599
HappyMan17 <sebasgrisales16@gmail.com>
2023-10-25 21:10:15 -0500

    feat(userService): user data and file added to a FormData to be send to the api.

--- src/api/service/userService.ts
+++ src/api/service/userService.ts
@@ -22,9 +22,15 @@ export const createUser = async (user: User): Promise<any | null> => {
   return data
 }
 
-export const updateUser = async (user: UserUpdate): Promise<any | null> => {
+export const updateUser = async (user: UserUpdate, files: any): Promise<any | null> => {
   const url = `${k.api.BASE_URL}${k.api.UPDATE}`
-  const { data, error } = await put(url, user)
+  const formData = new FormData();
+  Object.keys(user).forEach(key => { formData.append(key, user[key]); })
+  for (let i = 0; i < files.files.length; i++) {
+    formData.append('files', files.files[i]);
+  }
+
+  const { data, error } = await put(url, formData)
   if (error != null) {
     console.log('error updating user')
     return null
