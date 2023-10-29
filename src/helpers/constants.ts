ac56c0fddbe8ad5569441d7bb394db2a89a7694a
HappyMan17 <sebasgrisales16@gmail.com>
2023-10-29 16:39:53 -0500

    feat(UserService): get user function created.

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -3,6 +3,7 @@ const constants = {
     BASE_URL: 'http://localhost:3000',
     LOGIN: '/api/auth/login',
     REGISTER: '/api/auth/register',
+    GET_USER: '/api/user/',
     UPDATE: '/api/user/update',
     UPDATE_USER_PROFILE_IMAGE: '/api/user/upload',
     USER_PROFILE_IMAGE: '/profile_images/'
