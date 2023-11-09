1ab84c3ffe8a7285538cd6ecc477934f21d029b4
AndreaSoCa <andreasoca1212@outlook.es>
2023-11-08 19:58:00 -0500

    feat(userHome): Modificación del menú desplegable a lista con divisiones

--- src/components/data.ts
+++ src/components/data.ts
@@ -2,27 +2,31 @@ import { type NavItemType } from './types';
 
 export const userHomeNavItems: NavItemType[] = [
   {
-    buttonName: 'Profile',
-    dropdownButtonsName: [
-      { buttonName: 'Modify Profile', pageLink: 'modify-account' },
-      { buttonName: 'Inactivate Profile', pageLink: 'inactivate-account' }
-    ],
-    pageLink: ''
+    buttonName: 'Modify Profile',
+    pageLink: 'modify-account'
   },
   {
-    buttonName: 'Contacts',
-    dropdownButtonsName: [
-      { buttonName: 'Add Contacts', pageLink: 'add-contact' },
-      { buttonName: 'Delete Contacts', pageLink: 'delete-contact' }
-    ],
-    pageLink: ''
+    buttonName: 'Inactivate Profile',
+    pageLink: 'inactivate-account'
   },
   {
-    buttonName: 'Events',
-    dropdownButtonsName: [
-      { buttonName: 'Add Event', pageLink: 'add-event' },
-      { buttonName: 'Modify Event', pageLink: 'modify-event' }
-    ],
+    buttonName: 'Add Contacts',
+    pageLink: 'add-contact'
+  },
+  {
+    buttonName: 'Delete Contacts',
+    pageLink: 'delete-contact'
+  },
+  {
+    buttonName: 'Add Event',
+    pageLink: 'add-event'
+  },
+  {
+    buttonName: 'Modify Event',
+    pageLink: 'modify-event'
+  },
+  {
+    buttonName: 'User Home',
     pageLink: ''
   }
 ]
--- src/components/menu_component/MenuComponent.tsx
+++ src/components/menu_component/MenuComponent.tsx
@@ -1,56 +1,40 @@
-import { useEffect, useState } from 'react'
-import { Menu, MenuItem } from '@mui/material';
+import { useState } from 'react';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import Divider from '@mui/material/Divider';
 import { type NavItemType } from '../types';
 import { useNavigate } from 'react-router-dom';
 
 interface MenuComponentProps {
-  menuId: string
   buttonNameList: NavItemType[]
-  anchorEl: null | HTMLElement
-  handleClose: () => void
   toggleHandler?: () => void
 }
 
-export const MenuComponent: React.FC <MenuComponentProps> = ({
-  menuId,
-  buttonNameList,
-  anchorEl,
-  handleClose,
-  toggleHandler = () => null
-}) => {
-  const [isOpen, setIsOpen] = useState(false)
-  const navigate = useNavigate()
+export const MenuComponent: React.FC<MenuComponentProps> = ({ buttonNameList, toggleHandler = () => null }) => {
+  const [selectedIndex, setSelectedIndex] = useState<number | null>(null);
+  const navigate = useNavigate();
 
-  useEffect(() => {
-    if (anchorEl && anchorEl?.id === menuId) {
-      setIsOpen(true)
-    }
-  }, [anchorEl])
-
-  const handleIsOpen = (pageLink: string) => {
-    setIsOpen(false)
-    toggleHandler()
-    navigate(pageLink)
-  }
+  const handleItemClick = (index: number, pageLink: string) => {
+    setSelectedIndex(index);
+    toggleHandler();
+    navigate(pageLink);
+  };
 
   return (
-    <Menu
-      id="basic-menu"
-      anchorEl={anchorEl}
-      open={isOpen}
-      onClose={handleClose}
-      MenuListProps={{
-        'aria-labelledby': 'basic-button'
-      }}
-    >
-      {buttonNameList.map(item => (
-        <MenuItem
-          key={item.buttonName}
-          onClick={() => { handleIsOpen(item.pageLink) }}
-        >
-          {item.buttonName}
-        </MenuItem>
+    <List>
+      {buttonNameList.map((item, index) => (
+        <div key={item.buttonName}>
+          <ListItem
+            button
+            selected={index === selectedIndex}
+            onClick={() => { handleItemClick(index, item.pageLink); }}
+          >
+            <ListItemText primary={item.buttonName} />
+          </ListItem>
+          {index < buttonNameList.length - 1 && <Divider />}
+        </div>
       ))}
-    </Menu>
-  )
-}
+    </List>
+  );
+};
