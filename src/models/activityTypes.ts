213539e509d098e7e1dcee88ad2530c96b9b94e0
AndreaSoCa <andreasoca1212@outlook.es>
2023-11-14 12:53:58 -0500

    feat(): Actualizar activityService para incluir los últimos cambios en la API, Ajustar constantes y activityTypes para mayor consistencia

--- src/models/activityTypes.ts
+++ src/models/activityTypes.ts
@@ -6,3 +6,12 @@ export interface Activity {
   is_by_percentage: boolean
   has_been_done?: boolean
 }
+
+export interface ActivityUpdate {
+  activity_id: string
+  event_id: string
+  description: string
+  total_activity_value: number
+  is_by_percentage: boolean
+  has_been_done: boolean
+}
